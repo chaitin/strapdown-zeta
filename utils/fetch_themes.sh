a53f8bfcc1c8f47404a4adcb1209e24a37f2b56d
Pandada8 <pandada8@gmail.com>
2015-07-21 20:17:56 +0800

    add the new theme and update the theme fetch script

--- utils/fetch_themes.sh
+++ utils/fetch_themes.sh
@@ -1,19 +1,12 @@
 #!/bin/bash
-while read line; do
-    curl $line/bootstrap.min.css > `basename $line`.min.css
-done <<< "http://bootswatch.com/2/amelia/
-http://bootswatch.com/2/cerulean/
-http://bootswatch.com/2/cosmo/
-http://bootswatch.com/2/cyborg/
-http://bootswatch.com/2/flatly/
-http://bootswatch.com/2/journal/
-http://bootswatch.com/2/readable/
-http://bootswatch.com/2/simplex/
-http://bootswatch.com/2/slate/
-http://bootswatch.com/2/spacelab/
-http://bootswatch.com/2/spruce/
-http://bootswatch.com/2/superhero/
-http://bootswatch.com/2/united/"
+# Run the js in the index page to get the list
+# $('.section-preview .container:first .preview h3').map(function(n,x){return $(x).text()})
+themes=(Cerulean Cosmo Cyborg Darkly Flatly Journal Lumen Paper Readable Sandstone Simplex Slate Spacelab Superhero United Yeti)
+for i in "${themes[@]}"; do
+    i=`echo "$i" | tr 'A-Z' 'a-z'`
+    echo "https://bootswatch.com/$i/bootstrap.min.css"
+    curl -L  "https://bootswatch.com/$i/bootstrap.min.css" > "$i.min.css"
+done
 
-curl "http://bootswatch.com/2/default/bootstrap-responsive.min.css" > bootstrap-responsive.min.css
-curl "http://bootswatch.com/2/default/bootstrap.min.css" > bootstrap.min.css
+# curl "http://bootswatch.com/default/bootstrap-responsive.min.css" > bootstrap-responsive.min.css
+# curl "http://bootswatch.com/default/bootstrap.min.css" > bootstrap.min.css
