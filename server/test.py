1eae461c70beadd772bdc288bfb0947f42462a41
zTrix <i@ztrix.me>
2015-09-04 22:56:43 +0800

    update test.py, use self.url to construct url, support test_xxx from command line

--- server/test.py
+++ server/test.py
@@ -12,6 +12,7 @@ import requests
 import time
 import shutil
 
+CWD = os.path.dirname(os.path.realpath(__file__))
 
 def check_port(p):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -29,6 +30,12 @@ class Test(unittest.TestCase):
         f = open(os.path.join(self.cwd, fp), 'w')
         f.write(s)
         f.close()
+
+    def url(self, urlpath):
+        ret = "http://127.0.0.1:%d" % random.choice(self.ports)
+        if not urlpath.startswith('/'):
+            ret += '/'
+        return ret + urlpath
 
     def setUp(self):
         self.cwd = tempfile.mkdtemp()
@@ -66,12 +73,12 @@ class Test(unittest.TestCase):
     def test_basic(self):
         text = u"This is a test"
         self.writefile(".md", text)
-        r = requests.get("http://127.0.0.1:%d/" % self.ports[0])
+        r = requests.get(self.url('/'))
         self.assertIn(self.title, r.text)
         self.assertIn(unicode(text), r.text)
         self.assertGreater(len(r.text), len(text))
 
-        r = requests.get("http://127.0.0.1:%d/_static/version" % self.ports[1])
+        r = requests.get(self.url("/_static/version"))
         self.assertEqual(r.text, open(os.path.join(self.cwd, "_static", "version")).read())
         self.assertRegexpMatches(r.text, r'\d+\.\d+\.\d+(-\w+)?(\+[0-9A-Fa-f]{7,20})?')
 
@@ -86,7 +93,7 @@ class Test(unittest.TestCase):
 
     def test_normal_post(self):
         text = u"This is a test"
-        url = "http://127.0.0.1:%d/test" % self.ports[0]
+        url = self.url("/test")
         r = requests.get(url)
         self.assertIn("edit.min.js", r.text)
 
@@ -114,7 +121,7 @@ class Test(unittest.TestCase):
     def test_dir_issue(self):
         folder1_name = random_name()
         folder1 = os.path.join(self.cwd, folder1_name)
-        url = "http://127.0.0.1:%d/" % self.ports[0]
+        url = self.url("/")
 
         r = requests.get(url+folder1_name+"/")
         self.assertIn("edit.min.js", r.text)
@@ -140,7 +147,7 @@ class Test(unittest.TestCase):
     def test_upload(self):
         randomFile = os.urandom(20)
         filename = random_name() + '.mp4'
-        r = requests.post("http://127.0.0.1:%d/%s" % (self.ports[0], filename), files={
+        r = requests.post(self.url(filename), files={
             "body": (filename, randomFile)
         })
         self.assertEqual(r.content, randomFile)
@@ -150,7 +157,7 @@ class Test(unittest.TestCase):
     def test_upload_without_ext(self):
         randomFile = os.urandom(20)
         filename = random_name()
-        r = requests.post("http://127.0.0.1:%d/%s" % (self.ports[0], filename), files={
+        r = requests.post(self.url(filename), files={
             "body": (filename, randomFile)
         })
         self.assertEqual(r.content, randomFile)
@@ -159,25 +166,37 @@ class Test(unittest.TestCase):
 
     def test_content_type_for_static(self):
         self.writefile("www.css", "xxx")
-        r = requests.get("http://127.0.0.1:%d/www.css" % self.ports[0])
+        r = requests.get(self.url("/www.css"))
         self.assertEqual(r.headers['Content-Type'], "text/css; charset=utf-8")
 
     def test_upload_option_json(self):
-        r = requests.post("http://127.0.0.1:%d/test.option.json" % self.ports[0], data={
+        r = requests.post(self.url("/test.option.json"), data={
             "body": "some words"
         }, allow_redirects=False)
         self.assertGreater(r.status_code, 300)
         self.assertLess(r.status_code, 400)
 
-
 if __name__ == '__main__':
-    if os.path.dirname(sys.argv[0]):
-        os.chdir(os.path.dirname(sys.argv[0]))
+    os.chdir(CWD)
     suite = unittest.TestLoader().loadTestsFromTestCase(Test)
+
+    # for command line tests
+    # usage: ./test.py test_basic test_xxx test_xxx2
+    tests = []
+
+    if len(sys.argv) > 1:
+        tests.extend(sys.argv[1:])
+
+    if len(tests):
+        suite = unittest.TestSuite(map(Test, tests))
+
     rs = unittest.TextTestRunner(verbosity=2).run(suite)
+    try:
+        for i in tmpfolders:
+            shutil.rmtree(i)
+    except:
+        pass
     if len(rs.errors) > 0 or len(rs.failures) > 0:
         sys.exit(10)
     else:
-        for i in tmpfolders:
-            shutil.rmtree(i)
         sys.exit(0)
